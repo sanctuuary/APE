9d52a2f46d1eeb557751bdc90cc7f4c68e5b1481
Vedran Kasalica <v.kasalica@uu.nl>
2019-11-25 02:38:28 +0100

    Index update

---
+++ src/main/java/nl/uu/cs/ape/sat/LocalRun.java
@@ -0,0 +1,74 @@
+package nl.uu.cs.ape.sat;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.List;
+
+import org.json.JSONException;
+
+import guru.nidi.graphviz.attribute.RankDir;
+import nl.uu.cs.ape.sat.core.solutionStructure.SolutionWorkflow;
+import nl.uu.cs.ape.sat.utils.APEUtils;
+
+public class LocalRun {
+
+	public static void main(String[] args) {
+
+		String path = "/home/vedran/ownCloud/PhD/All Use Cases/Evaluation/UseCase4/";
+		String fileName = "ape.configuration";
+		if (!APEUtils.isValidReadFile(path + fileName)) {
+			System.err.println("Bad path.");
+			return;
+		}
+
+		File file = null;
+		try {
+			file = File.createTempFile("temp", null);
+			file.deleteOnExit();
+			String content = APEUtils.readFile(path + fileName, Charset.defaultCharset());
+			content = content.replace("./", path);
+			APEUtils.write2file(content, file, false);
+
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+
+		APE apeFramework = null;
+		try {
+			apeFramework = new APE(file.getAbsolutePath());
+		} catch (JSONException e) {
+			System.err.println("Error in parsing the configuration file.");
+			return;
+		} catch (IOException e) {
+			System.err.println("Error in reading the configuration file.");
+			return;
+		}
+		List<SolutionWorkflow> solutions;
+		try {
+			solutions = apeFramework.runSynthesis(file.getAbsolutePath());
+		} catch (IOException e) {
+			System.err.println("Error in synthesis execution. Writing to the file system failed.");
+			return;
+		}
+
+		/*
+		 * Writing solutions to the specified file in human readable format
+		 */
+		if (solutions.isEmpty()) {
+			System.out.println("UNSAT");
+		} else {
+			try {
+				apeFramework.writeSolutionToFile(solutions);
+				apeFramework.generateDataFlowGraphs(solutions, RankDir.TOP_TO_BOTTOM);
+//				apeFramework.generateControlFlowGraphs(solutions, RankDir.LEFT_TO_RIGHT);
+				apeFramework.executeWorkflows(solutions);
+			} catch (IOException e) {
+				System.err.println("Error in writing the solutions. to the file system.");
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+}
---
+++ src/main/java/nl/uu/cs/ape/sat/models/AllPredicates.java
@@ -0,0 +1,125 @@
+/**
+ * 
+ */
+package nl.uu.cs.ape.sat.models;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import nl.uu.cs.ape.sat.models.logic.constructs.TaxonomyPredicate;
+import nl.uu.cs.ape.sat.utils.APEUtils;
+
+/**
+ * The {@code AllPredicates} class is used to 
+ *
+ * @author Vedran Kasalica
+ *
+ */
+public abstract class AllPredicates extends HashMap<String, TaxonomyPredicate>{
+
+	/** Root of the taxonomy. */
+	private String taxonomyRoot;
+	
+	public AllPredicates(String dataTaxonomyRoot) {
+		this.taxonomyRoot = dataTaxonomyRoot;
+	}
+	
+	/**
+	 * Returns the root predicate of the taxonomy.
+	 * 
+	 * @return The root predicate.
+	 */
+	public TaxonomyPredicate getRootPredicate() {
+		return this.get(taxonomyRoot);
+	}
+	
+	/**
+	 * Set the root predicate of the taxonomy.
+	 * 
+	 */
+	public void setRootPredicate(TaxonomyPredicate root) {
+		this.taxonomyRoot = root.getPredicateID();
+	}
+	
+	/**
+	 * Returns the ID of the root predicate of the taxonomy.
+	 * 
+	 * @return The root predicate.
+	 */
+	public String getRootID() {
+		return taxonomyRoot;
+	}
+	
+	/**
+	 * Returns a list of final predicates.
+	 * 
+	 * @return list of types
+	 */
+	private List<TaxonomyPredicate> getAllNonEmptyPredicates() {
+
+		List<TaxonomyPredicate> allNonEmptyTypes = new ArrayList<TaxonomyPredicate>();
+		for (TaxonomyPredicate type : this.values()) {
+			if (!(type.isEmptyPredicate() || type.isRootPredicate())) {
+				allNonEmptyTypes.add(type);
+			}
+		}
+
+		return allNonEmptyTypes;
+	}
+	
+	/**
+	 * 
+	 */
+	public boolean trimTaxonomy() {
+		TaxonomyPredicate root = this.get(taxonomyRoot);
+		List<String> toRemove = new ArrayList<String>();
+		for(String subClassID : APEUtils.safe(root.getSubPredicates())) {
+			TaxonomyPredicate subClass = this.get(subClassID);
+			if(subClass == null) {
+				toRemove.add(subClassID);
+				continue;
+			}
+			if(subClass.getIsRelevant()) {
+				trimSubTaxonomy(subClass);
+			} else {
+				toRemove.add(subClassID);
+				trimSubTaxonomy(subClass);
+			}
+		}
+		root.removeAllSubPredicates(toRemove);
+		return true;
+	}
+	
+	public boolean trimSubTaxonomy(TaxonomyPredicate subTaxRoot) {
+		if(subTaxRoot == null) {
+			return true;
+		}
+		List<String> toRemove = new ArrayList<String>();
+		for(String subClassID : APEUtils.safe(subTaxRoot.getSubPredicates())) {
+			TaxonomyPredicate subClass = this.get(subClassID);
+			if(subClass == null) {
+				toRemove.add(subClassID);
+				continue;
+			}
+			if(subClass.getIsRelevant()) {
+				trimSubTaxonomy(subClass);
+			} else {
+				toRemove.add(subClassID);
+				trimSubTaxonomy(subClass);
+			}
+		}
+		if(subTaxRoot.getIsRelevant()) {
+			subTaxRoot.removeAllSubPredicates(toRemove);
+		} else {
+			this.remove(subTaxRoot.getPredicateID());
+		}
+		
+		return true;
+	}
+	
+	
+	
+	
+	
+}
