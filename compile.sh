619e21ea8c7be1ede39c2cbcdcfdc4f591ac8fe2
Vedran Kasalica <v.kasalica@uu.nl>
2020-02-13 03:37:29 +0100

    Compile script fixed

--- compile.sh
+++ compile.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-mvn clean install
-rm /home/vedran/ownCloud/PhD/All Use Cases/Test_UseCases/Demo_UseCase/APE-0.1.jar
-cp /home/vedran/git/APE/target/APE-0.1-jar-with-dependencies.jar home/vedran/ownCloud/PhD/All Use Cases/Test_UseCases/Demo_UseCase/APE-0.1.jar
+mvn clean -DskipTests=true install
+
