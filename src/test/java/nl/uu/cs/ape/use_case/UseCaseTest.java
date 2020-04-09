0d7fb26de43a34b6afb17ce6e3643e0b81cf0817
Maurin Voshol <maurinvoshol@gmail.com>
2020-04-09 18:08:16 +0200

    #15 Travis-CI (#17)
    Added .travis.yml to the repository and added an empty unit test

---
+++ src/test/java/nl/uu/cs/ape/use_case/UseCaseTest.java
@@ -0,0 +1,14 @@
+package nl.uu.cs.ape.use_case;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UseCaseTest {
+
+	@Test
+	void test() {
+		System.out.println("First unit test!");
+	}
+
+}
