86460dfb349f9ff1a33d05bea5683ae57afea4d5
Maurin Voshol <j.m.voshol@students.uu.nl>
2020-08-27 14:38:01 +0200

    debug

--- src/main/java/nl/uu/cs/ape/sat/configuration/APEConfig.java
+++ src/main/java/nl/uu/cs/ape/sat/configuration/APEConfig.java
@@ -2,6 +2,7 @@ package nl.uu.cs.ape.sat.configuration;
 
 import nl.uu.cs.ape.sat.utils.APEUtils;
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 public abstract class APEConfig {
 
@@ -17,5 +18,13 @@ public abstract class APEConfig {
 
     public JSONArray getAllTagInfoJSON(){
         return new JSONArray(APEUtils.map(getAllTags(), APEConfigTag::toJSON));
+    }
+
+    public void debug(){
+        final JSONObject json = new JSONObject();
+        for(APEConfigTag<?> tag : getAllTags()){
+            json.put(tag.getTagName(), tag.getValue().toString());
+        }
+        System.out.println(json.toString(3));
     }
 }
